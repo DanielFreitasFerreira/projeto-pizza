140264806225ee50267f7e1b69afefef983d69a2
Daniel Freitas Ferreira <danielfferreirabr@gmail.com>
2021-01-30 00:23:11 -0300

    (script): taking information and printing on the screen

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+const query = (el) => document.querySelector(el);
+const all = (el) => document.querySelectorAll(el);
+
 pizzaJson.map((item, index) => {
-  let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
+  let pizzaItem = query('.models .pizza-item').cloneNode(true);
+
+  pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+  pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+  pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+  pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
   
   // preencher as informações
-  document.querySelector('.pizza-area').append(pizzaItem);
+  query('.pizza-area').append(pizzaItem);
 
 });
+
+
+
