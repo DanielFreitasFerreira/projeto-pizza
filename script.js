9e6a160f271e6232b4e123842b35742c5af35053
Daniel Freitas Ferreira <danielfferreirabr@gmail.com>
2021-02-03 16:25:24 -0300

    (script): listing elements and doing attribute get

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ const all = (el) => document.querySelectorAll(el);
 pizzaJson.map((item, index) => {
   let pizzaItem = query('.models .pizza-item').cloneNode(true);
 
+  pizzaItem.setAttribute('data-key', index);
   pizzaItem.querySelector('.pizza-item--img img').src = item.img;
   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
   pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
   pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
   pizzaItem.querySelector('a').addEventListener('click', (e) => {
     e.preventDefault();
-    console.log("Clicou na pizza!");
+    const key = e.target.closest('.pizza-item').getAttribute('data-key');
+    console.log("Pizza clicada" + key);
 
     query('.pizzaWindowArea').style.opacity = 0;
     query('.pizzaWindowArea').style.display = 'flex';
